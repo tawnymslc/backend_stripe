b69569834fdbf10375da1cb7d0048e74a1889d7d
Tawny Mathi <tawnymslc@gmail.com>
2023-10-29 15:02:51 -0600

    trying again updated code per Stripe

--- index.js
+++ index.js
@@ -10,11 +10,12 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.post("/payment", async (req, res) => {
-  const intent = stripe.paymentIntents.create({
+  const paymentIntent = await stripe.paymentIntents.create({
     amount: 5000,
     currency: "usd",
-  })
+  });
+
   res.json({clientSecret: paymentIntent.client.secret});
-  })
+});
 
 app.listen(4243, () => console.log('Running on port 4243'));
