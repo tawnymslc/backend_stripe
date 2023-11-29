80dd0b11922f77179c62e2384483e59437b314ee
Tawny Mathi <tawnymslc@gmail.com>
2023-11-28 17:44:54 -0700

    updated server to include 2nd project with Spotify API

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const axios = require('axios');
+require('dotenv').config();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use(cors())
+
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
 
 app.post('/payment', async (req, res) => {
   const {paymentMethodType, currency, amount} = req.body;
@@ -25,5 +30,35 @@ app.post('/payment', async (req, res) => {
     res.status(400).json({error: { message: e.message}});
   }
 });
+
+app.post('/get-access-token', async (req, res) => {
+  try {
+      const accessToken = await obtainAccessToken();
+      console.log(accessToken);
+      res.json({ accessToken });
+  } catch (error) {
+      console.error('Error obtaining access token:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+async function obtainAccessToken() {
+  try {
+      const response = await axios.post(
+          'https://accounts.spotify.com/api/token',
+          'grant_type=client_credentials',
+          {
+              headers: {
+                  'Authorization': 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64'),
+                  'Content-Type': 'application/x-www-form-urlencoded'
+              },
+          }
+      );
+      return response.data.access_token;
+  } catch (error) {
+      console.error('Error obtaining access token:', error);
+      throw error; // Rethrow the error for proper handling in the calling function
+  }
+}
 
 app.listen(4243, () => console.log('Running on port 4243'));
